600cf7639398c2abaaef1b0f2ec13263b22b14d2
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-29 16:56:46 +0530

    "check_style_score -> 0 of 5 passed. style score: 0/1 "

---
+++ m3/Assignment-2/Solution.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+/*
+	Do not modify this main function.
+	*/
+public class Solution {
+/* Fill the main function to print the number of 7's between 1 to n*/
+    public static void main(String[] args) {
+
+        Scanner s=new Scanner(System.in);      
+        int n = s.nextInt();
+        int count = 0;
+        for(int i=1;i <= n;i++) {
+            int num = i;
+            while(num != 0) {
+        	if ((i % 10) == 7) count = count + 1;
+            num = num / 10;
+            }
+        }
+        System.out.println(count);
+    }
+}
