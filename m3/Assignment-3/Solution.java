acdb941c0488f5a4b40cb0ddb8420197b55f36c1
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-29 18:34:09 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-3/Solution.java
+++ m3/Assignment-3/Solution.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 /*
     Do not modify this main function.
     */
-public class Solution {
+class Solution {
+    private Solution() { }
 
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
         int n1 = s.nextInt();
         int n2 = s.nextInt();
-        
         System.out.println(gcd(n1, n2));
     }
     /*
