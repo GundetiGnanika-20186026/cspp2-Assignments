8d0473d8e045dc37fc53d6786dfaa631cf632ceb
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-29 17:09:42 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m3/Assignment-2/Solution.java
+++ m3/Assignment-2/Solution.java
@@ -10,18 +10,16 @@ class Solution {
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int count = 0;
-        for(int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             int num = i;
             final int ten = 10;
             final int seven = 7;
-            while(num != 0) {
+            while (num != 0) {
             if ((num % ten) == seven) {
              count = count + 1;
             }
-
             num = num / ten;
             }
-
         }
         System.out.println(count);
     }
