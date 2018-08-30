329455931f1fef1a158933e171100d079d3978d6
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-30 14:49:19 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -8,7 +8,7 @@ public class Solution {
 		int size1 = scn.nextInt();
 		int size2 = scn.nextInt();
 		arr1 = new int[size1][size2];
-		for (int i = 0;i<size1;i++)
+		for (int i = 0; i < size1; i++)
 			for(int j = 0; j < arr1[i].length; j++)
 				arr1[i][j] = scn.nextInt();
         
@@ -19,7 +19,7 @@ public class Solution {
 			for(int j = 0; j < arr2[i].length; j++)
 				arr2[i][j] = scn.nextInt();
         
-        if ((size1 != size11) || (size2 !=size22)) System.out.println("not possible");
+        if ((size1 != size11) || (size2 != size22)) System.out.println("not possible");
 		else {
         for (int i = 0; i < size1; i++)
 			for(int j = 0; j < arr1[i].length; j++)
@@ -29,7 +29,7 @@ public class Solution {
 		for (int i = 0; i < size11; i++) {
 			for(int j = 0; j < arr1[i].length; j++) {
 				System.out.print(arr1[i][j]);
-				if (j == 0 || j==1) System.out.print(" ");
+				if (j == 0 || j == 1) System.out.print(" ");
 			}
 			System.out.println();
 		}
