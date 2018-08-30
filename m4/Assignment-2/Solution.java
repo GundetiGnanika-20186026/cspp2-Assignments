4fd7f27118ea5898a1cf7841999432119ec251b7
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-30 14:39:29 +0530

    "check_style_score -> 2 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -23,15 +23,16 @@ public class Solution {
 			for(int j = 0; j < arr1[i].length; j++)
 				arr1[i][j] = arr1[i][j] + arr2[i][j];
         
-		if ((size1 != size11) || (size2 !=size22)) System.out.println("not posible");
-		else {
+		if ((size1 == size11) && (size2 == size22)) {
 		for (int i = 0; i < size11; i++) {
 			for(int j = 0; j < arr1[i].length; j++) {
 				System.out.print(arr1[i][j]);
 				if (j == 0 || j==1) System.out.print(" ");
 			}
 			System.out.println();
-		}
-	}
+		 }
+	    }
+	    else System.out.println("not possible");
+
 	}
 }
