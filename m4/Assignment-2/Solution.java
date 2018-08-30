57d3488b3f12c934edc12a438d90de5a539c1770
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-30 14:12:26 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

--- m4/Assignment-2/Solution.java
+++ m4/Assignment-2/Solution.java
@@ -23,8 +23,11 @@ public class Solution {
 			for(int j = 0;j<arr1[i].length;j++)
 				arr1[i][j] = arr1[i][j]+arr2[i][j];
         
-		for (int i = 0;i<size11;i++)
-			for(int j = 0;j<arr1[i].length;j++)
-				System.out.print(arr1[i][j]);
+		for (int i = 0;i<size11;i++) {
+			for(int j = 0;j<arr1[i].length;j++) {
+				System.out.print(arr1[i][j]+" ");
+			}
+			System.out.println();
+		}
 	}
 }
