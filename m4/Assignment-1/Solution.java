e7d1ad2cfc0b3f26a37aaa48c1a69959978dc9f2
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-30 12:26:23 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m4/Assignment-1/Solution.java
+++ m4/Assignment-1/Solution.java
@@ -1,10 +1,11 @@
-
 import java.util.*;
-public class Solution
+final class Solution
 {/*
 	Fill this main function to print maximum of given array
 	*/
-	public static void main(String[] args)
+	private Solution() { }
+
+	public static void main(final String[] args)
 	{
 		Scanner sc = new Scanner(System.in);
 		int size = sc.nextInt();
