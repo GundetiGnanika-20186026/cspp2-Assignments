4fa87b46fc4004acfaaa26e2004ea54e9f930c5f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-31 15:37:40 +0530

    "check_style_score -> 1 of 1 passed. style score: 0/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -1,22 +1,29 @@
-
-// Given an String, Write a java method that returns the decimal value for the given binary string.
-
-
 import java.util.*;
-public class Solution
-{/*
-	Do not modify this main function.
+/**
+ * converting binary to decimal.
+ */
+public class Solution{
+    /*
+	constructor.
 	*/
-	public static void main(String[] args)
+	private Solution() { }
+	/** 
+	 * @param args [description]
+	*/
+	public static void main(final String[] args)
 	{
-		Scanner sc=new Scanner(System.in);
-		int n=Integer.parseInt(sc.nextLine());
-		for(int i=0;i<n;i++) {
+		Scanner sc = new Scanner(System.in);
+		int n = Integer.parseInt(sc.nextLine());
+		for(int i = 0; i < n; i++) {
 			String s = sc.nextLine();
 			String res = binaryToDecimal(s);//Write binaryToDecimal function
 			System.out.println(res);
 		}
 	}
+	/**
+	 * @param s [passing string as a parameter]
+	 * @return [ a string in form of decimal ]
+	 */
 	static String binaryToDecimal(final String s) {
         int sum = 0;
         for (int i = 0 ,j = s.length() - 1; i < s.length(); i++, j--) {
@@ -24,7 +31,6 @@ public class Solution
         	String was =Character.toString(val);
         	int k = Integer.parseInt(was);
         	sum += k * Math.pow(2, j);
-
         }
         String result = Integer.toString(sum);
         return result;
