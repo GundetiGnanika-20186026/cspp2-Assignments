286a0df19a3db1271dfe2e9b4830c3e7665fbb95
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-31 15:47:11 +0530

    "check_style_score -> 1 of 1 passed. style score: 1/1 "

--- m4/Assignment-3/Solution.java
+++ m4/Assignment-3/Solution.java
@@ -3,36 +3,36 @@ import java.util.Scanner;
  * converting binary to decimal.
  */
 final class Solution {
-	/**
-	 * @constructor.
-	 */
-	private Solution() { }
-	/**
-	 * @param args [description]
-	*/
-	public static void main(final String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < n; i++) {
-			String s = sc.nextLine();
-			String res = binaryToDecimal(s);
-			System.out.println(res);
-		}
-	}
-	/**
-	 * @param s [passing string as a parameter]
-	 * @return [ a string in form of decimal ]
-	 */
-	static String binaryToDecimal(final String s) {
+    /**
+     * @constructor.
+     */
+    private Solution() { }
+    /**
+     * @param args [main method]
+    */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            String res = binaryToDecimal(s);
+            System.out.println(res);
+        }
+    }
+    /**
+     * @param s [passing string as a parameter]
+     * @return [ a string in form of decimal ]
+     */
+    static String binaryToDecimal(final String s) {
         int sum = 0;
         for (int i = 0, j = s.length() - 1; i < s.length(); i++, j--) {
-        	char val = s.charAt(i);
-        	String was = Character.toString(val);
-        	int k = Integer.parseInt(was);
-        	sum += k * Math.pow(2, j);
+            char val = s.charAt(i);
+            String was = Character.toString(val);
+            int k = Integer.parseInt(was);
+            sum += k * Math.pow(2, j);
         }
         String result = Integer.toString(sum);
         return result;
-	}
+    }
 
 }
