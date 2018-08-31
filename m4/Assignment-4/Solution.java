2749e5ba73b244ff188b70917ab7c99899f3e52d
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-08-31 14:03:32 +0530

    "check_style_score -> 5 of 5 passed. style score: 1/1 "

--- m4/Assignment-4/Solution.java
+++ m4/Assignment-4/Solution.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 /**
-* Given an String, Write a java method that returns the decimal value for the given binary string.
+* Given an String, We need to covrert to reverse.
 **/
 final class Solution {
-    /*
-    Do not modify the main function.
-    */
+    /**
+     * constructor.
+     */
     private Solution() { }
     /**
      * @param args [main method args]
