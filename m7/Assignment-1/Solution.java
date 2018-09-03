321a980ef0fb5702db13967693de5efc22b0aff8
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-03 18:16:49 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,9 +1,8 @@
 import java.util.*;
-class InputValidator
-{
+class InputValidator {
 	String data;
 	int length;
-    InputValidator(final String input){
+    InputValidator(final String input) {
 		data = input;
 		length = input.length();
 	}
@@ -15,10 +14,8 @@ class InputValidator
 		}
     }
 }
-public class Solution
-{
-	public static void main(final String args[])
-    {
+public class Solution {
+	public static void main(final String[] args) {
     	Scanner s = new Scanner(System.in);
     	String input = s.next();
     	InputValidator i = new InputValidator(input);    	
