568e4c15fe422ef1668912525ce7fbf405449c0a
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-03 18:20:24 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- m7/Assignment-1/Solution.java
+++ m7/Assignment-1/Solution.java
@@ -1,25 +1,25 @@
 import java.util.Scanner;
 class InputValidator {
-	String data;
-	int length;
+    String data;
+    int length;
     InputValidator(final String input) {
-		data = input;
-		length = input.length();
-	}
+        data = input;
+        length = input.length();
+    }
     public  boolean validateData() {
-    	final int six = 6;
-		if (length >= six) {
-			return true;
-		} 
-		return false;
+        final int six = 6;
+        if (length >= six) {
+            return true;
+        }
+        return false;
     }
 }
 public class Solution {
-	public static void main(final String[] args) {
-    	Scanner s = new Scanner(System.in);
-    	String input = s.next();
-    	InputValidator i = new InputValidator(input);    	
-    	System.out.println(i.validateData());
+    public static void main(final String[] args) {
+        Scanner s = new Scanner(System.in);
+        String input = s.next();
+        InputValidator i = new InputValidator(input);
+        System.out.println(i.validateData());
 
     }
 
