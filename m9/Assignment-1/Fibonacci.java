0574e1086e53f5b0dcbee065dea4833df4538704
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-05 10:32:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 0/1 "

--- m9/Assignment-1/Fibonacci.java
+++ m9/Assignment-1/Fibonacci.java
@@ -18,7 +18,7 @@ import java.util.Scanner;
  *
  */
 
-final class Fibonacci {
+class Fibonacci {
     /*
      * fib is a static method takes a argument n
      * n is the count of the fibonacci numbers to be generated.
