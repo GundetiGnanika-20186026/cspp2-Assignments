e4a9efcd6b6e7dc35f8e6a8d196e0a73b3bef4bb
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-06 13:34:38 +0530

    "check_style_score -> 2 of 6 passed. style score: 0/1 "

--- m10/Assignment-3/List.java
+++ m10/Assignment-3/List.java
@@ -61,7 +61,7 @@ public class List<E> {
         //int[] list1 = new list1[2*size]
         //java.lang.System.arraycopy(list, 0, list1, 0, size);
         //list = list1;
-        list = Arrays.copyOf(list, 2 * list.length);
+        list = Arrays.copyOf(list, 2 * size);
     }
     /*
      * The size method returns the value of the size.
