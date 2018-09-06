c3032f2828a6521ccac489cbefaed6a2147f55f3
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-06 16:46:57 +0530

    "check_style_score -> 6 of 6 passed. style score: 0/1 "

--- m10/Assignment-3/Solution.java
+++ m10/Assignment-3/Solution.java
@@ -108,7 +108,7 @@ public class Solution {
 	if (tokens.length == 2) {
 	String[] t1 = tokens[1].split(",");
 	Integer[] temp = new Integer[t1.length];
-	for (int i = 0; i < temp.length; i++ ) {
+	for (int i = 0; i < temp.length; i++) {
 	temp[i] = Integer.parseInt(t1[i]);
 	}
 	listInteger.addAll(temp);
@@ -160,8 +160,8 @@ public class Solution {
     case "addAll":
 	if (tokens.length == 2) {
 	String[] t1 = tokens[1].split(",");
-	Float temp[] = new Float[t1.length];
-	for (int i = 0; i < t1.length ; i++ ) {
+	Float[] temp = new Float[t1.length];
+	for (int i = 0; i < t1.length; i++) {
     temp[i] = Float.parseFloat(t1[i]);
 	}
 	listFloat.addAll(temp);
@@ -214,7 +214,7 @@ switch (tokens[0]) {
 	if (tokens.length == 2) {
 	String[] t1 = tokens[1].split(",");
 	Character[] temp = new Character[t1.length];
-	for (int i = 0; i < t1.length ; i++ ) {
+	for (int i = 0; i < t1.length; i++) {
 	temp[i] = t1[i].charAt(0);
 	}
 	listCharacter.addAll(temp);
@@ -323,7 +323,7 @@ switch (tokens[0]) {
 	if (tokens.length == 2) {
 	String[] t1 = tokens[1].split(",");
 	Student[] temp = new Student[t1.length];
-	for (int i = 0; i < t1.length ; i++ ) {
+	for (int i = 0; i < t1.length; i++) {
 	temp[i] = new Student(t1[i]);
 	}
 	listStudent.addAll(temp);
