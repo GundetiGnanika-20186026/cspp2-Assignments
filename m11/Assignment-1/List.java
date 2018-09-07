c60382cf60d6f89d58d1f634aa8d7f01dc6f0b2f
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-07 17:55:17 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m11/Assignment-1/List.java
+++ m11/Assignment-1/List.java
@@ -433,7 +433,7 @@ public class List {
                     l.removeAll(a);
                 }
                 break;
-            case "subList": {
+            case "subList":
                 if (tokens.length != 2) {
                     break;
                 }
@@ -444,7 +444,7 @@ public class List {
                     System.out.println(object);
                 }
                 break;
-            }
+
             case "equals":
                 if (tokens.length == 2) {
                     String[] lt = tokens[1].split(",");
