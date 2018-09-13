d564941bee2e842940493d2b40e7937261ea9bc8
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-13 13:07:14 +0530

    "check_style_score -> 4 of 4 passed. style score: 1/1 "

--- m16/bookyourshow/Patron.java
+++ m16/bookyourshow/Patron.java
@@ -1,6 +1,15 @@
+/**
+ * Class for patron.
+ */
 public class Patron {
 	String name;
 	String mobile;
+	/**
+	 * Constructs the object.
+	 *
+	 * @param      name    The name
+	 * @param      mobile  The mobile
+	 */
 	Patron(final String name, final String mobile) {
 		this.name = name;
 		this.mobile = mobile;
