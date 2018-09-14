51e217cf80aaafcd8ddad7613a5b073927d18675
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-14 10:17:11 +0530

    "check_style_score -> 0 of 4 passed. style score: 0/1 "

---
+++ m17/Shoppingcart/Solution.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+class Item {
+	String productname;
+	double quantity;
+	double unitprice;
+	Item(String pname,double qmeasure,double umoney) {
+       this.productname = pname;
+       this.quantity = qmeasure;
+       this.unitprice = umoney;
+	}
+}
+class Shoppingcart {
+	Item[] catalogarray;
+	Item[] cartarray;
+    int sizecatalog;
+	int sizecart;
+	Shoppingcart(){
+		catalogarray = new Item[50];
+		cartarray = new Item[50];
+		sizecatalog = 0;
+		sizecart = 0;
+
+	}
+	public void addToCatalog(Item item){
+		catalogarray[sizecatalog++] = item;
+
+	}
+	public void addTOCart(Item item){
+		cartarray[sizecart++] = item;
+
+    }
+	// public void removeFromCart(Item item){
+
+	// }
+	// public void showCart(){
+
+	// }
+	// public void showCatalog(){
+
+	// }
+	// public double getTotalAmount(){
+
+	// }
+	// public double getPayableAmount(){
+
+	// }
+}
+class Solution {
+	Solution() {
+
+	}
+	public static void main(String[] args) {
+		Shoppingcart obj = new Shoppingcart();
+		Scanner scn = new Scanner(System.in);
+        while(scn.hasNext()){
+        	String[] tokens = scn.nextLine().split(" ",2);
+        	switch(tokens[0]){
+        		case "Item":
+        		String[] items = tokens[1].split(",");
+        		Item itemobj = new Item(items[0],Double.parseDouble(items[1]),Double.parseDouble(items[2]));
+        		obj.addToCatalog(itemobj);
+        		default:
+
+        	}
+        }
+	}
+}
