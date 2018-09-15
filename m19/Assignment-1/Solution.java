f2c572280d76f53c5aebf1f1c184557ce1377195
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-15 12:51:46 +0530

    "check_style_score -> 2 of 13 passed. style score: 0/1 "

--- m19/Assignment-1/Solution.java
+++ m19/Assignment-1/Solution.java
@@ -77,20 +77,16 @@ public final class Solution {
         	    }
         	    else if ((Integer.parseInt(tockens[4]) < 0)){
            	     System.out.println("Invalid penalty for "+tockens[0]);
-           	    }
-           	 }
-
-
-        //        } else{
-        //         quiz.questionsarray[i] = tockens[0]+"("+tockens[3]+")";
-        //         quiz.choicesarray[i] = (Arrays.toString((tockens[1]).split(",")));
-        //         quiz.length = quiz.length + 1;
-        //          quiz.size = quiz.size+1;
-        //       }
-        //   }
-        //  if(tockenssize){
-        //    System.out.println(questionCount+" are added to the quiz");
-        // }
+           	    }else{
+                quiz.questionsarray[i] = tockens[0]+"("+tockens[3]+")";
+                quiz.choicesarray[i] = (Arrays.toString((tockens[1]).split(",")));
+                quiz.length = quiz.length + 1;
+                 quiz.size = quiz.size+1;
+              }
+          }
+         if(tockenssize){
+           System.out.println(questionCount+" are added to the quiz");
+        }
      }
 
     }
