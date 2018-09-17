721d1013b1f3a7034061ed13e8c41c858d1c32ad
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-17 11:47:05 +0530

    "check_style_score -> 5 of 13 passed. style score: 0/1 "

--- m20/Assignment-1/Solution.java
+++ m20/Assignment-1/Solution.java
@@ -275,7 +275,7 @@ public final class Solution {
                     }
 
         	        Question obj = new Question(first[0], first[1].split(","), Integer.parseInt(first[2]), Integer.parseInt(first[3]), Integer.parseInt(first[4]));
-                    quiz.addQuestion(obj);
+                    //quiz.addQuestion(obj);
             }
             if(res){
             	System.out.println(q+" are added to the quiz");
