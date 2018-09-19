14d06579ea4752b455aa868e02edd50713a30d7e
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-19 22:33:21 +0530

    social network

--- social/Solution.java
+++ social/Solution.java
@@ -1,92 +1,79 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-class User {
-	private String username;
-	private ArrayList<String> followers;
-	User(String name1,ArrayList<String>  followers1){
-		this.username = name1;
-		this.followers = followers1;
-
-	}
-	public String getusername() {
-		return username;
-	}
-
-	public ArrayList<String> getfollowers() {
-		return followers;
-	}
-}
-
-class Network {
-   ArrayList<User> userarray;
-   Network(){
-   	userarray = new ArrayList<User>();
-   }
-
-   public void addConnection(User obj1){
-   	    boolean required = false;
-   	    for(int i = 0; i < userarray.size();i++){
-   	    	if(userarray.get(i).username.equals(obj1.username)) {
-   	    		for(int j = 0; j < obj1.followers.size();j++)
-   	    			userarray.get(i).followers.add(obj1.followers.get(j));
-   	    		    required = true
-   	    	}
-   	    }
-   	    if(!required){
-   	    	userarray.add(obj1);
-   	    }
-   }
-
-   public void getConnections(String adval) {
-   	for(int i = 0;i<userarray.size();i++) {
-   		if(userarray.get(i).)
-   	}
-   }
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-class Solution{
+import java.util.*;
+import java.io.*;
+class Solution {
+	static TreeMap<String, ArrayList<String>> dict = new TreeMap<>();
 	public static void main(String[] args) {
-		Scanner scn = new Scanner(System.in);
-		while(scn.hasNext()){
-			// String[] original = scn.nextLine().split("\\.");
-			// String[] initial = original[0].split(" is connected to ");
-			// System.out.println(Arrays.toString(initial));
-			Network obj = new Network()
-			String original = scn.nextLine().replace("("," ").replace(")","");
-			String[] initial = original.split(" ",2);
-			switch(initial[0]){
-				case "addConnection":
-				    String flist = initial[1];
-				    ArrayList<String> fnew;
-				    fnew = new ArrayList<String>(initial.length-2);
-				    for(int i=2;i<initial.length;i++){
-				    	fnew.add(initial[i]);
-				    }
-				    obj.addConnection(new User(flist,fnew));
-				    break;
-				case:"getConnections":
-				    obj.getConnections(initial[1]);
-				    break;
-				case "getcommonConnections":
-				    obj.getcommonConnections(initial[1],initial[2]);
-                    break;
+		try {
+			File file = new File("input000.txt");
+			Scanner scn = new Scanner(file);
+			String[] limit = scn.nextLine().split(" ");
+			for (int i = 0; i < Integer.parseInt(limit[1]); i++) {
+				String line = scn.nextLine().replace(".", "");
+				String[] tokens = line.split(" is connected to ");
+				String[] follower = tokens[1].split(", ");
+				//System.out.println(followers);
+				ArrayList<String> followerslist = new ArrayList<String>(Arrays.asList(follower));
+				dict.put(tokens[0], followerslist);
+			}
+			while (scn.hasNext()) {
+				String[] test = scn.nextLine().split(" ");
+				switch (test[0]) {
+				case "Network":
+					//
+					// System.out.println(dict);
+					// System.out.println();
+					String[] keys = dict.keySet().toArray(new String[dict.keySet().size()]);
+					for(int i = 0;i<keys.length-1;i++){
+						System.out.print(keys[i]+": ");
+						System.out.print(dict.get(keys[i])+", ");
+					}
+					System.out.print(keys[keys.length-1] +": ");
+					System.out.print(dict.get(keys[keys.length-1]));
+					System.out.println();
+					System.out.println();
+					break;
+				case "addConnections":
+					if (dict.containsKey(test[1])) {
+						dict.get(test[1]).add(test[2]);
+					}
+					break;
+				case "getConnections":
+					if (dict.containsKey(test[1])) {
+						System.out.println(dict.get(test[1]));
+						System.out.println();
+					} else {
+						System.out.println("Not a user in Network");
+						System.out.println();
+					}
+					break;
+				case "CommonConnections" :
+					ArrayList<String> common = new ArrayList<>();
+					ArrayList<String> first = dict.get(test[1]);
+					ArrayList<String> second = dict.get(test[2]);
+					for (int i = 0; i < first.size(); i++) {
+						for (int j = 0; j < second.size(); j++) {
+							if (first.get(i).equals(second.get(j))) {
+								common.add(first.get(i));
+							}
+						}
+					}
+					System.out.println(common);
+					System.out.println();
+					break;
 
 
+
+				}
 			}
 
+
+		} catch (FileNotFoundException e) {
+			System.out.println("No such file");
 		}
 	}
+
+
+
+
+
 }
