37703b22f50ee2b86a7ff442a6f089cb21d9fd46
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-20 17:30:35 +0530

    "check_style_score -> 0 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -77,7 +77,8 @@ class Solution {
 
                     // File file2 = new File("test2.txt");
                     Scanner scn1 = new Scanner(files[j]);
-                    String s2 = scn1.nextLine().replaceAll("[^ A-Za-z0-9]","");
+                    //String s2 = scn1.nextLine().replaceAll("[^ A-Za-z0-9]","");
+                    String s2 = scn1.nextLine().replaceAll("[^ \\p{Alpha}]","");
                     String[] para2 = s2.toLowerCase().split(" ");
                     // String[] para2 = scn1.nextLine().split(" ");
                     HashMap<String, Integer> final2 = Countfreq(para2);
