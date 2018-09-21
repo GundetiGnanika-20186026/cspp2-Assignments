b9ac4b95b985fd7406b31f5660fbd96cac4dbdff
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-21 14:26:58 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- m23/Assignment-1/Solution.java
+++ m23/Assignment-1/Solution.java
@@ -8,28 +8,28 @@ class Bagofwords {
 	 * { var_description }
 	 */
 	private  HashMap<String, Integer> dict1;
-    /**
-     * Constructs the object.
-     */
+	/**
+	 * Constructs the object.
+	 */
 	Bagofwords() {
 		dict1 = new HashMap<>();
 	}
-    /**
-     * { function_description }
-     *
-     * @return     { description_of_the_return_value }
-     */
+	/**
+	 * { function_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public HashMap<String, Integer> getdict1() {
 		return dict1;
 	}
-    /**
-     * { function_description }
-     *
-     * @param      finald1  The finald 1
-     * @param      finald2  The finald 2
-     *
-     * @return     { description_of_the_return_value }
-     */
+	/**
+	 * { function_description }
+	 *
+	 * @param      finald1  The finald 1
+	 * @param      finald2  The finald 2
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public long plagerism(HashMap<String, Integer> finald1, HashMap<String, Integer> finald2) {
 		// HashMap<String, ArrayList<Integer>> result = new Hash;
 		//  for(int j = 0;j < finald2.size(); j++) {Map<>();
@@ -70,24 +70,24 @@ class Bagofwords {
 
 
 	// public void Stringmatch(HashMap<String, Integer> finald11, HashMap<String, Integer> finald22) {
- //    int lcs = 0;
+//    int lcs = 0;
 	// int max = 0;
- //    	for(String key1 : finald11.keySet()){
+//    	for(String key1 : finald11.keySet()){
 	//     	for(String key2 : finald22.keySet()){
- //    			if(key1.charAt(0).equals(key2.charAt(0))){
- //    				if(key1.equals(key2)) {
- //    					lcs = key1.length();
+//    			if(key1.charAt(0).equals(key2.charAt(0))){
+//    				if(key1.equals(key2)) {
+//    					lcs = key1.length();
 	//    				}
 
 
- //    			}
- //    			if (max < lcs)  {
- //    					max = lcs;
- //    				}
+//    			}
+//    			if (max < lcs)  {
+//    					max = lcs;
+//    				}
 
- //    		}
+//    		}
 
- //   	    }
+//   	    }
 
 
 	// }
@@ -104,13 +104,13 @@ class Solution {
 	 * Class for solution.
 	 */
 	Solution() {};
-    /**
-     * { function_description }
-     *
-     * @param      args  The arguments
-     */
+	/**
+	 * { function_description }
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(String[] args) {
-		int a = 0,b = 0;
+		int a = 0, b = 0;
 
 		try {
 			Scanner linescan = new Scanner(System.in);
@@ -121,13 +121,13 @@ class Solution {
 			// for(File i : files){
 			//     System.out.println(i);
 			// // }
-			System.out.print("\t"+"\t");
-            for (File file : files){
-            	System.out.print(file.getName()+"\t");
-            }
-            System.out.println();
+			System.out.print("\t" + "\t");
+			for (File file : files) {
+				System.out.print(file.getName() + "\t");
+			}
+			System.out.println();
 			for (int i = 0; i < files.length; i++) {
-				System.out.print(files[i].getName()+"\t");
+				System.out.print(files[i].getName() + "\t");
 				for (int j = 0; j < files.length; j++) {
 					Bagofwords finalobj = new Bagofwords();
 
@@ -135,18 +135,18 @@ class Solution {
 					Scanner scn = new Scanner(files[i]);
 					HashMap<String, Integer> final1 = new HashMap<>();
 					String[] para1;
-					String s1="";
+					String s1 = "";
 					while (scn.hasNext()) {
 						//String s1 = scn.useDelimiter("\\A").next();
-					    String s11 = scn.nextLine().replaceAll("[^A-Za-z0-9_ ]"," ");
-					    s11 = s11.replaceAll("\\s+"," ");
-					    s1 = s1 +s11;
+						String s11 = scn.nextLine().replaceAll("[^A-Za-z0-9_ ]", " ");
+						s11 = s11.replaceAll("\\s+", " ");
+						s1 = s1 + s11;
 
 
 					}
 					para1 = s1.toLowerCase().split(" ");
-						final1 = Countfreq(para1);
-						int  keyscount = final1.keySet().size();
+					final1 = Countfreq(para1);
+					int  keyscount = final1.keySet().size();
 
 
 					// String s1 = scn.useDelimiter("\\A").next();//nextLine().replaceAll("[^ A-Za-z0-9]","");
@@ -164,11 +164,11 @@ class Solution {
 					// String s2 = scn1.nextLine().replaceAll("[^ \\p{Alpha}]","");
 					HashMap<String, Integer> final2 = new HashMap<>();
 					String[] para2;
-					String s2 ="";
+					String s2 = "";
 					while (scn1.hasNext()) {
 						//String s2 = scn1.useDelimiter("\\A").next();
-						String s22 = scn1.nextLine().replaceAll("[^A-Za-z0-9_ ]"," ");
-						s22 = s22.replaceAll("\\s+"," ");
+						String s22 = scn1.nextLine().replaceAll("[^A-Za-z0-9_ ]", " ");
+						s22 = s22.replaceAll("\\s+", " ");
 						s2 = s2 + s22;
 						//System.out.println(s2);
 						// para2 = s2.toLowerCase().split(" ");
@@ -182,10 +182,10 @@ class Solution {
 					scn1.close();
 					double resultpercent = finalobj.plagerism(final1, final2);
 					resultpercent = Math.round(resultpercent);
-                    double max = 0.0;
-					System.out.print((long)resultpercent+"\t\t");
+					double max = 0.0;
+					System.out.print((long)resultpercent + "\t\t");
 
-					if(resultpercent > max && i!=j) {
+					if (resultpercent > max && i != j) {
 						max = resultpercent;
 						a = i;
 						b = j;
@@ -194,7 +194,7 @@ class Solution {
 				}
 				System.out.println();
 			}
-			System.out.println("Maximum similarity is between "+files[a].getName()+" "+"and"+" "+files[b].getName());
+			System.out.println("Maximum similarity is between " + files[a].getName() + " " + "and" + " " + files[b].getName());
 
 
 
