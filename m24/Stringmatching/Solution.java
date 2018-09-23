909d08547957e0818b077d486a8ece0eb1495aac
GundetiGnanika-20186026 <gundetignanika425@msitprogram.net>
2018-09-23 17:33:47 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- m24/Stringmatching/Solution.java
+++ m24/Stringmatching/Solution.java
@@ -3,54 +3,7 @@ import java.util.Scanner;
 import java.io.File;
 
 
-class SubString1{
-    String s1 = "";
-    String s2 = "";
-    double res;
-    SubString1() {
-    }
-    public double plagerism(File file1, File file2){
-        try {
-            Scanner scn = new Scanner(file1);
-            while (scn.hasNext()) {
-                s1 += scn.nextLine();
-            }
-            scn = new Scanner(file2);
-            while (scn.hasNext()) {
-                s2 += scn.nextLine();
-            }
-            String longsub = "";
-            for(int i = 0;i<s2.length();i++){
-                for(int j = i+1;j<=s2.length();j++){
-                    if(s1.contains(s2.substring(i,j))){
-                        if (s2.substring(i,j).length() > longsub.length()){
-                            longsub = s2.substring(i,j);
-                        }
-                    }
-                }
-            }
-            if(file1.getName().equals(file2.getName())){
-                res = 100;
 
-                // System.out.print("\t\t");
-            }
-            else {
-                double num = longsub.length()*2*100;
-                double den = s1.length()+s2.length();
-                res = Math.round(num/den);
-
-
-            }
-             System.out.format("%13.1f",res);
-
-        } catch (Exception e) {
-            System.out.println("FILE NOT FOUND");
-        }
-        return res;
-
-    }
-
-}
 public final  class Solution {
     /**
      * Class for solution.
@@ -74,33 +27,33 @@ public final  class Solution {
             String file22 = "";
 
             System.out.print("             ");
-            for(int i = 0;i < files.length - 1;i++) {
+            for (int i = 0; i < files.length - 1; i++) {
                 System.out.print(files[i].getName() + "    ");
             }
             System.out.print(files[files.length - 1].getName() + " ");
             System.out.println();
             for (int i = 0; i < files.length; i++) {
                 System.out.print(files[i].getName());
-                    for (int j = 0; j < files.length; j++) {
-                        SubString1 obj = new SubString1();
-                        double result = obj.plagerism(files[i],files[j]);
+                for (int j = 0; j < files.length; j++) {
+                    SubString1 obj = new SubString1();
+                    double result = obj.plagerism(files[i], files[j]);
 
-                        if(result > maximum && !files[i].getName().equals(files[j].getName())){
-                            maximum = result;
-                            file11=files[i].getName();
-                            file22 = files[j].getName();
-
-                        }
+                    if (result > maximum && !files[i].getName().equals(files[j].getName())) {
+                        maximum = result;
+                        file11 = files[i].getName();
+                        file22 = files[j].getName();
 
                     }
-                    System.out.print(" ");
-                    System.out.println();
+
+                }
+                System.out.print(" ");
+                System.out.println();
 
             }
             System.out.println("Maximum similarity is between "
-             + file11 + " " + "and" + " " + file22);
+                               + file11 + " " + "and" + " " + file22);
         } catch (Exception e) {
             System.out.println("Empty Directory");
         }
-}
+    }
 }
